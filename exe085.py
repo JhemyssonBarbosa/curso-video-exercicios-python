b807906fd34715907f4012d8ca3eecfe302f6125
Jhemysson Barbosa <jhemyssonbarbosa@gmail.com>
2022-01-01 11:30:50 -0300

    exe085

---
+++ exe085.py
@@ -0,0 +1,25 @@
+par_impar = [[], []]
+for c in range(1, 10):
+    info = (int(input(f'Digite o {c}º valor: ')))
+    if info % 2 == 0:
+        par_impar[0].append(info)
+    else:
+        par_impar[1].append(info)
+print('Os valores PARES digitatos foram ', end='')
+for c, v in enumerate(sorted(par_impar[0])):
+    print(f'{v}', end='')
+    if c <= len(par_impar[0])-3:
+        print(', ', end='')
+    if c == len(par_impar[0])-2:
+        print(' e ', end='')
+    if c == len(par_impar[0])-1:
+        print('.')
+print('Os valores IMPARES digitados foram ', end='')
+for c, v in enumerate(sorted(par_impar[1])):
+    print(f'{v}', end='')
+    if c <= len(par_impar[1])-3:
+        print(', ', end='')
+    if c == len(par_impar[1])-2:
+        print(' e ', end='')
+    if c == len(par_impar[1])-1:
+        print('.')
